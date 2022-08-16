0db3f026f972d4f10ca735abdcc8ceb24ef04b87
Eugene Mefyod <96493615+heavenyoung1@users.noreply.github.com>
2022-08-16 19:08:22 +0300

    реализована возможность выхода и входа пользователя из учётной записи

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,7 +22,10 @@ urlpatterns = [
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
 
     # Todos
     path('current/', views.currenttodos, name='currenttodos'),
+    path('', views.home, name='home'),
 ]
