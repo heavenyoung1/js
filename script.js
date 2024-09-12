a5d6be4df72b4d74dd23dc60447ff93022666524
Evgeny <jacksonou3@gmail.com>
2024-09-12 19:16:44 +0300

    решение задачи

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-var animals = ["Кот", "Рыба", "Лемур", "Комодский варан"];
-for (var i = 0; i < animals.length; i++) {
-    console.log(animals[i] + " - прекрасное животное");
-    animals[i] = animals[i] + " - прекрасное животное";
-}
 
-console.log(animals)
+var strInput = "javascript is awesome";
+var strOutput = "";
+for (var i = 0; i < strInput.length; i++) {
+    if  (strInput[i] == "a") {
+        strOutput += "4";
+    }
+    else if  (strInput[i] == "e") {
+        strOutput += "3";
+    }
+    else if  (strInput[i] == "i") {
+        strOutput += "1";
+    }
+    else if  (strInput[i] == "o") {
+        strOutput += "0";
+    }
+    else {
+        strOutput += strInput[i];
+
+    }
+}
+console.log(strOutput);
