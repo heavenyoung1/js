8bb843a9053fb8ded3780f6d8c89a3e517dec780
Evgeny <jacksonou3@gmail.com>
2024-09-15 21:57:56 +0300

    Функция

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-var words = [
-    "программа",
-    "макака",
-    "прекрасный",
-    "оладушек"
-]
+var outFirstFunction = function () {
+    console.log("Hello, World!");
+};
 
-var word = words[Math.floor(Math.random()) * words.length]
+outFirstFunction();
 
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
 
-var remainingLetters = word.length
+// var words = [
+//     "программа",
+//     "макака",
+//     "прекрасный",
+//     "оладушек"
+// ]
 
-while (remainingLetters > 0) {
-    alert(answerArray.join(" "));
-    var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
-    if (guess == null) {
-        break;
-    } else if (guess !== 1) {
-        alert("Пожалуйста, введите только одну букву!")
-    }
-    for (var j = 0; j < word.length; j++) {
-        if (word[j] == guess) {
-            if (word[j] == guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
-            }
-        }
-    }
-}
-alert(answerArray.join(" "));
-alert("Отлично! Было загадано слово :" + word)
+// words.push("растение")
+
+// var word = words[Math.floor(Math.random()) * words.length]
+
+// var answerArray = [];
+// for (var i = 0; i < word.length; i++) {
+//     answerArray[i] = "_";
+// }
+
+// var remainingLetters = word.length
+
+// while (remainingLetters > 0) {
+//     alert(answerArray.join(" "));
+//     var guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры");
+//     if (guess == null) {
+//         break;
+//     } else if (guess !== 1) {
+//         alert("Пожалуйста, введите только одну букву!")
+//     }
+//     for (var j = 0; j < word.length; j++) {
+//         if (word[j] == guess.toLowerCase) {
+//             if (word[j] == guess) {
+//                 answerArray[j] = guess;
+//                 remainingLetters--;
+//             }
+//         }
+//     }
+// }
+// alert(answerArray.join(" "));
+// alert("Отлично! Было загадано слово :" + word)
 
 
 // var likeCats = confirm("Тебе нравятся кошки?");
