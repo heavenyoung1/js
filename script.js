57ca3a3489a57cb3a3fd56650d2074f7e4c76ebc
Evgeny <jacksonou3@gmail.com>
2024-12-31 13:59:26 +0300

    Любое непустое значение при преобразовании в тип данных Boolean даёт TRUE, пустое соответственно даёт False.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let nullToNr = null;
 nullToNr = Number(nullToNr);
 console.log("null", nullToNr, typeof nullToNr);
+
 let strToNr = "";
 strToNr = Number(strToNr);
 console.log("empty string", strToNr, typeof strToNr);
+
+let strToBool = "false";
+strToBool = Boolean(strToBool);
+console.log(strToBool, typeof strToBool);
+
+let strToBool2 = "";
+strToBool2 = Boolean(strToBool2);
+console.log(strToBool2, typeof strToBool2);
