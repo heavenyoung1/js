1d82b033ec4d215034f28abe965bd2088cf9a3ac
Evgeny <jacksonou3@gmail.com>
2024-12-31 14:06:29 +0300

    Почему typeof null == object? Риторический вопрос

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ let nr1 = 2;
 let nr2 = "2";
 console.log(nr1 + nr2);
 console.log(nr1 + Number(nr2));
+
+let str1 = "Laurence";
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log(
+    typeof str1,
+    typeof str2,
+    typeof val1,
+    typeof val2,
+    typeof myNum,
+);
