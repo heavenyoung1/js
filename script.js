ff71702a39018155119f7c6ed242280e12758621
Evgeny <jacksonou3@gmail.com>
2024-12-31 14:59:40 +0300

    Взаимодействие с операторами

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let strToBool2 = "";
 strToBool2 = Boolean(strToBool2);
 console.log(strToBool2, typeof strToBool2);
 
-let nr1 = 2;
+let nr1 = 200;
 let nr2 = "2";
 console.log(nr1 + nr2);
 console.log(nr1 + Number(nr2));
@@ -32,3 +32,26 @@ console.log(
     typeof val2,
     typeof myNum,
 );
+
+let myName = "Evgeny";
+let age = "25";
+let yesOrNot = true;
+console.log("Hello, my name is " + myName + " I am " + age + " years " + "and I can code JS: " + yesOrNot);
+
+let result1 = nr1 - Number(nr2);
+let result2 = str1 - nr1;
+console.log(result1, result2);
+
+let result3 = nr1 * Number(nr2);
+let result4 = nr1 * str1;
+console.log(result3, result4); 
+
+let result5 = nr1 / Number(nr2);
+console.log(result5);
+
+let quadro = nr1 ** Number(nr2);
+console.log(quadro);
+
+let balance = nr1 % 3;
+console.log(balance)
+
