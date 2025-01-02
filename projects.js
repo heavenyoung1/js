9a007f57fee0d79e2680922de4dd2fd83d94bb18
Evgeny <jacksonou3@gmail.com>
2025-01-02 19:32:05 +0300

    Решена задча ИМТ

--- projects.js
+++ projects.js
@@ -1,3 +1,11 @@
-let miles = prompt("Input length in miles");
-let kilometers = 1.60934 * miles;
-console.log(`Расстояние ${miles} миль равно ${kilometers} км.`)
+// let miles = prompt("Input length in miles");
+// let kilometers = 1.60934 * miles;
+// console.log(`Расстояние ${miles} миль равно ${kilometers} км.`)
+
+// Индекс массы тела
+
+let length = prompt("Введите рост в дюймах")
+let weight = prompt("Введите вес в фунтах")
+console.log(`Рост: ${length} дюймов. Вес: ${weight} фунтов.`)
+let imt = (weight * 0.453592) / ((length * 0.393701) ** 2) 
+console.log(`Ваш ИМТ: ${imt}`)
