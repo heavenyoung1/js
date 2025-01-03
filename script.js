b22bbaf8ff232582c5fd3fe42a9bef1f97a05320
Evgeny <jacksonou3@gmail.com>
2025-01-03 11:31:10 +0300

    Изучен метод concat()

--- script.js
+++ script.js
@@ -155,4 +155,9 @@ let arrOfShapes = [
 arrOfShapes.splice(2, 2, "square", "trapezoid");
 console.log(arrOfShapes);
 
-
+let arr5 = [1, 2, 3];
+let arr6 = [4, 5, 6];
+let arr7 = arr5.concat(arr6);
+console.log(arr7);
+let arr8 = arr7.concat(7, 8, 9);
+console.log(arr8);
