94b5e305b0338a104b48b7a21a2a508ea3a6c38f
Evgeny <jacksonou3@gmail.com>
2025-02-14 22:25:55 +0300

    Поправлен вызов функции getData1

--- script.js
+++ script.js
@@ -1626,7 +1626,10 @@ function build() {
     let el1 = document.body;
     let html = "<table>\n";
         for (let i = 0; i < nameArray1.length; i++) {
-            html += `<tr data-row="${i}" dataname="${nameArray1[i]}" onclick="${getData1(this)}"><td class="box">${nameArray1[i]}</td></tr>\n`;
+            html += `<tr data-row="${i}" dataname="${nameArray1[i]}" onclick="getData1(this)">
+                        <td class="box">${nameArray1[i]}</td>
+                    </tr>\n`;
+            
         } 
     html += "</table>";
     const container = document.getElementById("table-container");
